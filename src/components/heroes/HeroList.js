4634130734ecbd37884caa8a21d85977f7c069ac
Steven Andrade <stvn.piano@gmail.com>
2021-01-22 14:12:40 -0500

    navbar-links

---
+++ src/components/heroes/HeroList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { getHeroByPublisher } from '../../selectors/getHeroByPublisher'
+
+export const HeroList = ({ publisher }) => {
+
+    const heroes = getHeroByPublisher( publisher );
+    return (
+        <ul>
+            {
+                heroes.map( hero => (
+                    <li key={ hero.id }>
+                        {hero.superhero}
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
