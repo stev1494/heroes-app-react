dea26ce85e3bb25a3a99d22da14344c86adb3ba3
Steven Andrade <stvn.piano@gmail.com>
2021-01-22 15:12:38 -0500

    #	deleted:    public/assets/heroes/._marvel-spider.jpg

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { getHeroByPublisher } from '../../selectors/getHeroByPublisher'
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroByPublisher( publisher );
     return (
-        <ul>
+        <div className="card-columns">
             {
                 heroes.map( hero => (
-                    <li key={ hero.id }>
-                        {hero.superhero}
-                    </li>
+                    <HeroCard 
+                        key={ hero.id }
+                        {...hero}
+                    >
+                    </HeroCard>
                 ))
             }
-        </ul>
+        </div>
     )
 }
